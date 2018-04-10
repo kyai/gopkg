1b92d0681b84c1d5c74ba8743f9f36d2cc76d14a
kyai <hukuang@163.com>
2018-04-10 21:27:09 +0800

    commit

---
+++ goicon/goicon.go
@@ -0,0 +1,62 @@
+package goicon
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"regexp"
+	"strings"
+)
+
+func Get(u string) (string, error) {
+	var ico string
+	// get real url
+	u = strings.ToLower(u)
+	// get domain
+	uparse, err := url.Parse(u)
+	if err != nil {
+		return ico, err
+	}
+	domain := uparse.Scheme + "://" + uparse.Host
+
+	var resp *http.Response
+
+	// try to get it
+	// if resp, err = http.Get(domain + "/favicon.ico"); err == nil && resp.StatusCode == 200 {
+	// 	return domain + "/favicon.ico", err
+	// }
+
+	resp, err = http.Get(domain)
+	if err != nil {
+		fmt.Println(err)
+		return ico, err
+	}
+
+	b, err := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		fmt.Println("err2")
+		return ico, err
+	}
+
+	str := string(b)
+	reg := regexp.MustCompile(`(?i:<link ).+(?i:href\s*=\s*"|'\s*)(.+)("|')`)
+	// regHref := regexp.MustCompile(``)
+	// fmt.Printf("%q\n", reg.FindAllString(str, -1))
+	for _, v := range reg.FindAllString(str, -1) {
+		fmt.Println(v)
+		// ico = regHref.FindStringSubmatch(str)[1]
+	}
+	return ico, err
+
+	// return ico, err
+}
+
+func verifyUrl(url string) error {
+	if resp, err := http.Get(url); err == nil && resp.StatusCode == 200 {
+		return nil
+	}
+	return errors.New("Error")
+}
---
+++ gopw/gopw.go
@@ -0,0 +1,78 @@
+package gopw
+
+import (
+	"errors"
+	"math/rand"
+	"time"
+)
+
+var (
+	Len int
+	Str string
+)
+
+func init() {
+	level(0)
+}
+
+func level(i int) {
+	switch i {
+	case 0:
+		Len = 6
+		Str = char(0)
+	case 1:
+		Len = 8
+		Str = char(0) + char(1)
+	case 2:
+		Len = 8
+		Str = char(0) + char(1) + char(2)
+	default: // level 3 and more
+		Len = 10
+		Str = char(0) + char(1) + char(2) + char(3)
+	}
+}
+
+func char(i int) string {
+	switch i {
+	case 0:
+		return "0123456789"
+	case 1:
+		return "abcdefghijklmnopqrstuvwxyz"
+	case 2:
+		return "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	case 3:
+		return "@#$%^&*+-=_"
+	}
+	return ""
+}
+
+func Create(args ...interface{}) (string, error) {
+	var pwd string
+	var err error
+	if len(args) == 1 {
+		l, _ := args[0].(int)
+		level(l)
+	} else {
+		for _, arg := range args {
+			switch arg.(type) {
+			case int:
+				Len, _ = arg.(int)
+			case string:
+				Str, _ = arg.(string)
+			default:
+				return pwd, errors.New("Type error")
+			}
+		}
+	}
+	pwd = CreatePwd(Len, Str)
+	return pwd, err
+}
+
+func CreatePwd(l int, s string) string {
+	p := ""
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := 0; i < l; i++ {
+		p += string([]byte(s)[r.Intn(len(s))])
+	}
+	return p
+}
