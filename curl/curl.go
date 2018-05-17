0dd76cd741cc1d785221f622f9b6ba502c8ccaef
kyai <hukuang@163.com>
2018-05-17 10:55:24 +0800

    commit

--- curl/curl.go
+++ curl/curl.go
@@ -3,47 +3,64 @@ package curl
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
-func Do(method, url string, data map[string]interface{}, obj interface{}) error {
+func Do(method, url string, data map[string]interface{}, header map[string]string) (respData interface{}, err error) {
+
+	if method == "" {
+		err = errors.New("No method")
+		return
+	}
+	method = strings.ToUpper(method)
+
+	if header == nil {
+		header = make(map[string]string)
+		header["Content-Type"] = "application/json"
+	}
 
 	jsonS, err := json.Marshal(data)
 	if err != nil {
-		return err
+		return
 	}
 	reader := bytes.NewReader(jsonS)
 	request, err := http.NewRequest(method, url, reader)
 	if err != nil {
-		return err
+		return
 	}
 
-	request.Header.Set("Content-Type", "application/json")
+	for k, v := range header {
+		request.Header.Set(k, v)
+	}
 
 	httpC := http.Client{}
 
 	resp, err := httpC.Do(request)
 	if err != nil {
-		return err
+		return
 	}
 
 	rdata, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return err
+		return
 	}
 
-	if err = json.Unmarshal(rdata, obj); err != nil {
-		return err
+	if header["Content-Type"] == "application/json" {
+		if err = json.Unmarshal(rdata, &respData); err != nil {
+			return
+		}
 	}
 
-	return nil
+	return
 }
 
-func Get(url string, data map[string]interface{}, obj interface{}) {
-	Do("Get", url, data, obj)
+func Get(url string, data map[string]interface{}, header map[string]string) (respData interface{}, err error) {
+	return Do("Get", url, data, header)
 }
 
-func Post(url string, data map[string]interface{}, obj interface{}) {
-	Do("Post", url, data, obj)
+func Post(url string, data map[string]interface{}, header map[string]string) (respData interface{}, err error) {
+	return Do("Post", url, data, header)
 }
